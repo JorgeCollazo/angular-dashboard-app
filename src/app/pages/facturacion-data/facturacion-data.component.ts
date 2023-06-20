f1405cb675e03cdbf692c83f1cb40c8f2375df88
Elias Dominguez <edominguez@grupo-itl.com>
2023-06-20 16:03:16 -0500

    Login changed, added export and filters buttons UI

--- src/app/pages/facturacion-data/facturacion-data.component.ts
+++ src/app/pages/facturacion-data/facturacion-data.component.ts
@@ -2,7 +2,7 @@ import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable, MatTableDataSource } from '@angular/material/table';
-import { FacturacionDataDataSource, FacturacionDataItem } from './facturacion-data-datasource';
+import { FacturacionDataItem } from './facturacion-data-datasource';
 import { EXAMPLE_DATA } from './facturacion-data-datasource'
 
 @Component({
@@ -17,12 +17,18 @@ export class FacturacionDataComponent implements AfterViewInit {
 
   dataSource:  MatTableDataSource<FacturacionDataItem>;
 
+  EXAMPLE_DATA: FacturacionDataItem[] = [
+    {nro: 1, fecha_creacion: new Date(), fecha_inicio: new Date(), t_pago: 51, t_doc: 15, t_op: 71, status: 1, evento: 1, sucursal: 'Xtra'},
+    {nro: 2, fecha_creacion: new Date(), fecha_inicio: new Date(), t_pago: 52, t_doc: 25, t_op: 72, status: 2, evento: 2, sucursal: 'Campeon'},
+    {nro: 3, fecha_creacion: new Date(), fecha_inicio: new Date(), t_pago: 53, t_doc: 35, t_op: 73, status: 3, evento: 3, sucursal: 'El Costo'},
+    {nro: 4, fecha_creacion: new Date(), fecha_inicio: new Date(), t_pago: 54, t_doc: 45, t_op: 74, status: 4, evento: 4, sucursal: 'Oca Loca'},
+    {nro: 5, fecha_creacion: new Date(), fecha_inicio: new Date(), t_pago: 55, t_doc: 55, t_op: 75, status: 5, evento: 5, sucursal: 'Conway'},
+    {nro: 6, fecha_creacion: new Date(), fecha_inicio: new Date(), t_pago: 56, t_doc: 65, t_op: 76, status: 6, evento: 6, sucursal: 'Titan'},
+    {nro: 7, fecha_creacion: new Date(), fecha_inicio: new Date(), t_pago: 57, t_doc: 75, t_op: 77, status: 7, evento: 7, sucursal: 'Super 99'},
+  ]
+
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['nro', 'fecha_creacion', 'evento', 'sucursal', 't_pago', 't_doc', 't_op', 'status', 'actions'];
-
-  // dataSource = new MatTableDataSource(EXAMPLE_DATA)
-
-  // userColl = new MatTableDataSource<FacturacionDataItem>([]);
 
   constructor() {
     this.dataSource = new MatTableDataSource<FacturacionDataItem>;
@@ -40,8 +46,6 @@ export class FacturacionDataComponent implements AfterViewInit {
 
   applyFilter(event: Event): void {
     let filterValue = (event.target as HTMLInputElement)?.value;
-    console.log(filterValue);
-    console.log(this.dataSource);
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
